57433861092f6c2573e9eeb937cb5a3538cbf73c
notBigL <99298254+notBigL@users.noreply.github.com>
2024-01-12 21:12:28 +0100

    Initial Commit. Deleted everything out of the template I do not need. Console Command is in the game but does not work.

---
+++ src/main/java/code/commands/MakeStrikeCommand.java
@@ -0,0 +1,44 @@
+package code.commands;
+
+import basemod.DevConsole;
+import basemod.devcommands.ConsoleCommand;
+import basemod.helpers.ConvertHelper;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import java.util.ArrayList;
+
+public class MakeStrikeCommand extends ConsoleCommand {
+
+    public MakeStrikeCommand() {
+        this.requiresPlayer = true;
+        this.minExtraTokens = 1;
+        this.maxExtraTokens = 1;
+    }
+
+    public void execute(String[] tokens, int depth) {
+        if (tokens.length != 2) {
+            cmdDrawHelp();
+        } else {
+            AbstractDungeon.actionManager.addToTop(new DrawCardAction(AbstractDungeon.player, ConvertHelper.tryParseInt(tokens[1], 0)));
+        }
+    }
+
+    public ArrayList<String> extraOptions(String[] tokens, int depth) {
+        if (tokens[depth].matches("\\d+")) {
+            complete = true;
+        }
+
+        return ConsoleCommand.smallNumbers();
+    }
+
+    public void errorMsg() {
+        cmdDrawHelp();
+    }
+
+    private static void cmdDrawHelp() {
+        DevConsole.couldNotParse();
+        DevConsole.log("options are:");
+        DevConsole.log("* draw [amt]");
+    }
+}
