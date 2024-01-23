1b17f23c25aec07de8893838dc72ff0abbe96e2b
notBigL <99298254+notBigL@users.noreply.github.com>
2024-01-23 14:07:45 +0100

    Cards in all Piles can now be made strike. But the strike tag can not be taken away.

--- src/main/java/code/commands/MakeStrikeCommand.java
+++ src/main/java/code/commands/MakeStrikeCommand.java
@@ -3,7 +3,9 @@ package code.commands;
 import basemod.BaseMod;
 import basemod.DevConsole;
 import basemod.devcommands.ConsoleCommand;
+import basemod.helpers.CardTags;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 
 import java.util.ArrayList;
@@ -22,7 +24,31 @@ public class MakeStrikeCommand extends ConsoleCommand {
         String cardName = this.getCardID(tokens);
         AbstractCard c = CardLibrary.getCard(cardName);
         if (c != null) {
-            DevConsole.log(c.name);
+
+            for (AbstractCard handCard : new ArrayList<>(AbstractDungeon.player.hand.group)) {
+                if (c.cardID.equals(handCard.cardID) && !handCard.hasTag(AbstractCard.CardTags.STRIKE)) {
+                    CardTags.addTags(handCard, AbstractCard.CardTags.STRIKE);
+                    System.out.println("Strike tag added to" + handCard.cardID);
+                }
+            }
+            for (AbstractCard drawPileCard : new ArrayList<>(AbstractDungeon.player.drawPile.group)) {
+                if (c.cardID.equals(drawPileCard.cardID) && !drawPileCard.hasTag(AbstractCard.CardTags.STRIKE)) {
+                    CardTags.addTags(drawPileCard, AbstractCard.CardTags.STRIKE);
+                    System.out.println("Strike tag added to" + drawPileCard.cardID);
+                }
+            }
+            for (AbstractCard discardPileCard : new ArrayList<>(AbstractDungeon.player.discardPile.group)) {
+                if (c.cardID.equals(discardPileCard.cardID) && !discardPileCard.hasTag(AbstractCard.CardTags.STRIKE)) {
+                    CardTags.addTags(discardPileCard, AbstractCard.CardTags.STRIKE);
+                    System.out.println("Strike tag added to" + discardPileCard.cardID);
+                }
+            }
+            for (AbstractCard exhaustPileCard : new ArrayList<>(AbstractDungeon.player.exhaustPile.group)) {
+                if (c.cardID.equals(exhaustPileCard.cardID) && !exhaustPileCard.hasTag(AbstractCard.CardTags.STRIKE)) {
+                    CardTags.addTags(exhaustPileCard, AbstractCard.CardTags.STRIKE);
+                    System.out.println("Strike tag added to" + exhaustPileCard.cardID);
+                }
+            }
         } else {
             DevConsole.log("could not find card " + cardName);
         }
@@ -33,13 +59,13 @@ public class MakeStrikeCommand extends ConsoleCommand {
     }
 
     public void errorMsg() {
-        cmdDrawHelp();
+        cmdMakeStrikeHelp();
     }
 
-    private static void cmdDrawHelp() {
+    private static void cmdMakeStrikeHelp() {
         DevConsole.couldNotParse();
         DevConsole.log("options are:");
-        DevConsole.log("* draw [amt]");
+        DevConsole.log("* makestrike [id]");
     }
 
     private String getCardID(String[] tokens) {
